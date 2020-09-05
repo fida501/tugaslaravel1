577a17993fd7c44d1f9155ea4a84ecf0c852d1fd
fida501 <vivalafida123@gmail.com>
2020-09-05 11:58:07 +0700

    Tugas1
    Tugas1

---
+++ resources/views/test123.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test123</title>
+</head>
+<body>
+ 
+	<h3>Pepega</h3>
+    <p>Muhammad Fidaul Haq</p>
+    <p>XII RPL 2</p>
+	<p>Absen : 18</p>
+ 
+</body>
+</html>
